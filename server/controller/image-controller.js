93e8935ce3815885706b68d19e472c86f493bd54
Ansari Irfan <115148633+Ansari-Irfan-360@users.noreply.github.com>
2024-06-10 19:05:46 +0530

    Update image-controller.js

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 
-const url = 'https://reflect-ibvy.onrender.com';
+const url = 'https://reflect-360.onrender.com';
 
 
 let gfs, gridfsBucket;
